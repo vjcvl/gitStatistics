f4ec157712f41e716f134f2c11666ee151063a30
vijayakumar <90125077+vjcvl@users.noreply.github.com>
2023-04-26 16:40:38 +0530

    Update graphQl.js

--- src/graphQl.js
+++ src/graphQl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const openSource = {
-  githubConvertedToken: "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",
+  githubConvertedToken: "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",
   githubUserName: "vjcvl",
 };
 
